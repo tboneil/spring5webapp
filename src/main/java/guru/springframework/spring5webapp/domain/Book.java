533cf92594a3530acfe7808f5648f214036d254f
Tyler O'Neil <tyleroneil523@gmail.com>
2025-02-24 23:12:22 -0700

    Initialing the Sets with new Hash sets since we removed it from the constructor.

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.Entity;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import javax.persistence.*;
@@ -17,7 +18,7 @@ public class Book {
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = {@JoinColumn(name = "author_id")})
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
 
     public Book() {}
 
