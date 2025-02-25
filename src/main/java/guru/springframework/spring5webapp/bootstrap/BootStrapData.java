7b9d2796a8e949ef0fcce3892458d3757f3e351c
Tyler O'Neil <tyleroneil523@gmail.com>
2025-02-25 00:04:09 -0700

    Created Publisher class and repo to store publisher information

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootStrapData implements CommandLineRunner {
 
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(BookRepository bookRepository, AuthorRepository authorRepository) {
+    public BootStrapData(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository) {
         this.bookRepository = bookRepository;
         this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -23,6 +27,34 @@ public class BootStrapData implements CommandLineRunner {
 
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "12345");
+
+        // Adds book to eric and eric to the book
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        // Saving into the H2 Database
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnston");
+        Book noEJB = new Book("J2EE", "12345");
+
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        // Saving to H2 database
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        // Publisher creation
+        Publisher penguin = new Publisher("Penguin", "NoWhere Street", "Fairbanks", "AK", "99701");
+
+        // Add publisher to repo
+        publisherRepository.save(penguin);
+
+        // Console messaging output
+        System.out.println("Started in bootstrap");
+        System.out.println("Books found: " + bookRepository.count());
 
     }
 }
