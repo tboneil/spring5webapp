d63886d9ef07ecdefb090444ae793935bab91f0d
Tyler O'Neil <tyleroneil523@gmail.com>
2025-02-24 21:09:42 -0700

    Created Book class and populated methods.

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -0,0 +1,40 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.Set;
+
+public class Book {
+
+    private String title;
+    private String isbn;
+    private Set<String> authors;
+
+    public Book(String title, String isbn, Set<String> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<String> authors) {
+        this.authors = authors;
+    }
+}
